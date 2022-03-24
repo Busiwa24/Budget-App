dcbbd348a1a62f59efd8ba618ab03862362a08c8
Busiwa24 <busiwaliuma@gmail.com>
2022-03-24 15:29:11 +0200

    Add starter files

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  has_many :groups, dependent: :destroy
+  has_many :entities, dependent: :destroy
+  has_one_attached :avatar, dependent: :destroy
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, uniqueness: true
+end
